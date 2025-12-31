03d3ac32f6783522fd404da5d919f751ad1fd9c5
Alec <asungyang@gmail.com>
2025-12-31 21:42:30 +0100

    add prosemirror styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './style.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css";
+import App from "./App.tsx";
+import "@/richtext/core/extensions/nodes/table/table.css";
+import "@/richtext/core/extensions/marks/link/link.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
