b66c0f97b959e1d24ad7fd11b6202d2a00f5afb8
Alec <asungyang@gmail.com>
2026-02-10 01:12:47 +0100

    added method to get block data

--- src/lib/template/contexts/templateContext.svelte.ts
+++ src/lib/template/contexts/templateContext.svelte.ts
@@ -1,5 +1,5 @@
-import { SvelteMap } from "svelte/reactivity";
-import { type BlockCoordinates, type ColumnCoordinates, type NodeEntity, type TemplateEntity } from "../types";
+import { SvelteMap, SvelteSet } from "svelte/reactivity";
+import { type BlockCoordinates, type BlockEntity, type BlockType, type ColumnCoordinates, type NodeEntity, type TemplateEntity } from "../types";
 import { getContext, setContext } from "svelte";
 
 export class TemplateStore {
@@ -65,6 +65,17 @@ export class TemplateStore {
             return rows[blockCoords.rowIndex].columns[blockCoords.columnIndex].blocks[blockCoords.blockIndex];
 
         return null;
+    }
+
+    getBlocksByTypes(types: BlockType[]): Array<{ entity: BlockEntity, coordinates: BlockCoordinates }> {
+        const searchedTypes = new SvelteSet(types);
+        const matches = [];
+        for (const [, coordinates] of this._blocksIndex.entries()) {
+            const { rowIndex, columnIndex, blockIndex } = coordinates;
+            const entity = this.template.root.rows[rowIndex].columns[columnIndex].blocks[blockIndex];
+            if (searchedTypes.has(entity.type)) matches.push({ entity, coordinates });
+        }
+        return matches;
     }
 }
 
