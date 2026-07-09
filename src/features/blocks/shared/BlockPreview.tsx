683f7157bc86f47d0f764e05c3c83cdb88540d8b
Alec <asungyang@gmail.com>
2026-07-09 18:41:26 +0200

    Merge param className

--- src/features/blocks/shared/BlockPreview.tsx
+++ src/features/blocks/shared/BlockPreview.tsx
@@ -1,14 +1,22 @@
 import type { Block, PreviewProps } from "@/features/models/types";
 import { blockRegistry } from "@/features/blocks";
+import { cn } from "@/components/utils/cn";
 
-export const BlockPreview = ({ block, ref }: PreviewProps<Block>) => {
+export const BlockPreview = ({
+  block,
+  ref,
+  className,
+}: PreviewProps<Block>) => {
   const definition = blockRegistry[block.type];
   const { Icon, label } = definition;
 
   return (
     <div
       ref={ref}
-      className="flex aspect-square flex-col items-center justify-center rounded-lg border hover:bg-accent"
+      className={cn(
+        "flex aspect-square flex-col items-center justify-center rounded-lg border hover:bg-accent",
+        className,
+      )}
     >
       {Icon && <Icon />}
 
